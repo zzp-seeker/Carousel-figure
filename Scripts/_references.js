89f78a66a7fd14fbd0081868e2a48ced73e37230
zlzppq <957715602@qq.com>
2017-10-09 23:44:11 +0800

    first

---
+++ Scripts/_references.js
@@ -0,0 +1,3 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../js/accordion.js" />
+/// <reference path="jquery-3.1.1.min.js" />
---
+++ js/accordion.js
@@ -0,0 +1,5 @@
+﻿$(function () {
+    $(".text").mouseover(function () {
+        $(this).parent().stop(true).animate({"width":"600px"},1000).siblings().stop(true).animate({"width":"50px"},1000);
+    });
+});
